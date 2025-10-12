ef310d3c7434d6547d368e03a1dfd09ce67df841
nguyenquochung03 <nguyenhungnqh27@gmail.com>
2025-10-12 12:21:22 +0700

    Initalize Project

---
+++ CineGo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineGo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
